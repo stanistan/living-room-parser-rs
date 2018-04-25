6ab7aa54b5bdf4c08c3fe64526876b8b293a6be6
Stan Rozenraukh <stan@stanistan.com>
2018-04-25 12:14:03 -0400

    test against the json value instead of the enum

--- src/lib.rs
+++ src/lib.rs
@@ -39,15 +39,13 @@ impl <'a> Term<'a> {
     }
 }
 
-/// this macro implicitly uses the `Term` enum variants, to make it simpler to
-/// write terms.
-#[allow(unused_macros)]
-macro_rules! ts {
-    ($($t:expr),*) => {{
-        #[allow(unused_imports)]
-        use Term::*;
-        vec![$($t,)*]
-    }};
+use serde::ser::{Serialize, Serializer};
+impl <'a> Serialize for Term<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        self.repr_json().serialize(serializer)
+    }
 }
 
 mod grammar {
@@ -61,44 +59,63 @@ mod tests {
 
     use super::*;
 
-    macro_rules! assert_ts {
-        ($e: expr => [ $($t:expr),* ])  => {
-            assert_eq!(
-                ts![ $($t),* ],
-                grammar::parse($e).unwrap()
-            );
-        }
+    fn parse_to_serde_value(s: &str) -> serde_json::Value {
+        let terms = grammar::parse(s).unwrap();
+        let parsed: Vec<_> = terms.iter().map(|s| s.repr_json()).collect();
+        let parsed_as_json = serde_json::to_string(&parsed).unwrap();
+        serde_json::from_str(&parsed_as_json).unwrap()
     }
 
+    fn ws() -> serde_json::Value {
+        word(" ")
+    }
+
+    fn word(s: &str) -> serde_json::Value {
+        json!({ "word": s })
+    }
+
+    fn id(s: &str) -> serde_json::Value {
+        json!({ "id": s })
+    }
+
+    fn var(s: &str) -> serde_json::Value {
+        json!({ "variable": s })
+    }
+
+    fn value<T: serde::ser::Serialize>(v: T) -> serde_json::Value {
+        json!({ "value": v })
+    }
+
+    fn null() -> serde_json::Value {
+        json!({ "value": null })
+    }
+
+    fn wildcard() -> serde_json::Value {
+        json!({ "wildcard": true })
+    }
+
+    fn hole() -> serde_json::Value {
+        json!({ "hole": true })
+    }
+
+    /// Generates groups of named tests that validate an input string
+    /// against the generated json (and not an intermediate type).
+    ///
+    /// The expression gets parsed by the parser, then serialized, and
+    /// unserialized into JSON so we can do the comparison against
+    /// the final output.
     macro_rules! test_ts {
         ($( $n:ident [ $($e: expr => [ $($t:expr),* ]),* ] ),*) => {
             $(
                 #[test]
                 fn $n() {
-                    $(assert_ts!($e => [ $($t),* ]);)*
+                    $({
+                        let unparsed = parse_to_serde_value($e);
+                        assert_eq!(json!([ $($t),*]), unparsed);
+                    })*
                 }
             )*
         }
-    }
-
-    fn ws<'a>() -> Term<'a> {
-        Term::Whitespace
-    }
-
-    fn word(s: &str) -> Term {
-        Term::Word(s)
-    }
-
-    fn id(s: &str) -> Term {
-        Term::Id(s)
-    }
-
-    fn var(s: &str) -> Term {
-        Term::Variable(s)
-    }
-
-    fn string(s: &str) -> Term {
-        Term::String(s)
     }
 
     test_ts!(
@@ -110,77 +127,72 @@ mod tests {
             "#hi" => [ id("hi") ]
         ],
         test_values [
-            "0" => [ Int(0) ],
-            "1123" => [ Int(1123) ],
-            "-10" => [ Int(-10) ], "+1" => [ Int(1) ],
-            "true" => [ Bool(true) ],
-            "false" => [ Bool(false) ],
-            "null" => [ Null ],
-            "candy is null" => [ word("candy"), ws(), word("is"), ws(), Null ]
+            "0" => [ value(0) ],
+            "1123" => [ value(1123) ],
+            "-10" => [ value(-10) ], "+1" => [ value(1) ],
+            "true" => [ value(true) ],
+            "false" => [ value(false) ],
+            "null" => [ null() ],
+            "candy is null" => [ word("candy"), ws(), word("is"), ws(), null() ]
         ],
         test_vars [
             "gorog is at $x $y but _ sometimes $ 1" => [
                 word("gorog"), ws(), word("is"), ws(), word("at"), ws(),
                 var("x"), ws(), var("y"), ws(), word("but"), ws(),
-                Hole, ws(), word("sometimes"), ws(), Wildcard, ws(), Int(1)
+                hole(), ws(), word("sometimes"), ws(), wildcard(), ws(), value(1)
             ]
         ],
         test_oddities [
-            "a_" => [ word("a"), Hole ],
+            "a_" => [ word("a"), hole()],
             ",,," => [ word(",,,") ],
             "a,y" => [ word("a,y") ],
             "hi. you" => [ word("hi."), ws(), word("you") ],
-            "hi1" => [ word("hi"), Int(1) ],
-            "hi.1" => [ word("hi"), Float(0.1) ],
+            "hi1" => [ word("hi"), value(1) ],
+            "hi.1" => [ word("hi"), value(0.1) ],
             "#" => [ id("") ]
         ],
         test_coords [
             "($a, $b)" => [ word("("), var("a"), word(","), ws(), var("b"), word(")") ]
         ],
         test_nums [
-            "1 10 1.2" => [ Int(1), ws(), Int(10), ws(), Float(1.2) ],
-            "0.1" => [ Float(0.1) ],
-            ".1" => [ Float(0.1) ]
+            "1 10 1.2" => [ value(1), ws(), value(10), ws(), value(1.2) ],
+            "0.1" => [ value(0.1) ],
+            ".1" => [ value(0.1) ]
         ],
         test_strings [
-            "\"aay\"" => [ string("aay") ],
+            "\"aay\"" => [ value("aay") ],
             // "w\"" => [ word("w\"") ], FIXME SHOULD PANIC?
-            "w\"\"" => [ word("w"), string("") ]
+            "w\"\"" => [ word("w"), value("") ]
         ]
     );
 
-    macro_rules! to_json {
-        ($($e:expr),*) => {
-            serde_json::to_string(&vec![
-                $($e.repr_json()),*
-            ]).unwrap()
-        }
-    }
 
-    macro_rules! assert_json {
-        ([$($e:expr),*] => $t:tt) => {
-            assert_eq!(
-                format!("{}", json!($t)),
-                to_json!($($e),*)
-            );
-        }
-    }
-
+    /// Make sure we're serializing values to JSON the correct way.
     #[test]
     fn test_serialize_value() {
+
+        macro_rules! assert_json {
+            ([$($e:expr),*] => $t:tt) => {
+                assert_eq!(
+                    format!("{}", json!($t)),
+                    serde_json::to_string(&vec![ $($e),* ]).unwrap()
+                );
+            }
+        }
+
         assert_json!(
             [
                 Term::Bool(false),
                 Term::Float(10.0),
                 Term::Hole,
-                id("ay"),
+                Term::Id("ay"),
                 Term::Int(123),
                 Term::Null,
-                string("hi"),
-                var("sup"),
-                ws(),
+                Term::String("hi"),
+                Term::Variable("sup"),
+                Term::Whitespace,
                 Term::Wildcard,
-                word("banana")
+                Term::Word("banana")
             ]
             =>
             [
